808db17a6decee81bf841566e902926a68313a8a
Vichhai <kanvichhai@gmail.com>
2020-01-21 13:13:27 +0900

    - commit

---
+++ WebcashStore/Application/Main/RootViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RootViewController.swift
+//  WebcashStore
+//
+//  Created by kosign webcash on 1/20/20.
+//  Copyright © 2020 Webcash. All rights reserved.
+//
+
+import UIKit
+import RESideMenu
+
+class RootViewController: RESideMenu,RESideMenuDelegate {
+
+    override func loadView() {
+        super.loadView()
+        self.setupSideMenu()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    //MAKR: - custom methods
+    func setupSideMenu() {
+        let leftMenu    = self.VC(sbName: "SideMenu", identifier: "leftMenuViewController")
+        let mainVC    = self.VC(sbName: "Main", identifier: "MainViewController")
+        
+        self.contentViewScaleValue  = 0.9
+        self.leftMenuViewController = leftMenu
+        self.contentViewController  = mainVC
+        self.scaleMenuView = false
+    }
+
+}
