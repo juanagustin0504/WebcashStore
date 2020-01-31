d1034baf6d9f6ae08a41824fe49b5a4d69393b7d
juanagustin0504 <juanagustin@naver.com>
2020-01-31 11:11:25 +0900

    - add Settings Storyboard

---
+++ WebcashStore/Application/Settings/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  WebcashStore
+//
+//  Created by Webcash on 2020/01/30.
+//  Copyright © 2020 Webcash. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
