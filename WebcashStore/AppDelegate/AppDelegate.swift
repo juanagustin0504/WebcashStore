1a18574f322ca1fc37fb8752e683b4afdc2a1e54
juanagustin0504 <juanagustin@naver.com>
2020-01-31 17:21:58 +0900

    - add NotificationCenterDelegate, MessagingDelegate

--- WebcashStore/AppDelegate/AppDelegate.swift
+++ WebcashStore/AppDelegate/AppDelegate.swift
@@ -29,10 +29,100 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Firebase
         FirebaseApp.configure()
+        // Message
+        Messaging.messaging().delegate = self
+        
+
+        registerForRemoteNotifications(application)
         
         return true
     }
-
-
 }
 
+extension AppDelegate {
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        // let token = deviceToken.reduce("", {$0 + String(format: "%02X", $1)})
+        // Shared.Property.deviceToken = token
+        
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    // iOS 9
+    func application(_ application: UIApplication, didRegister notificationSettings: UIUserNotificationSettings) {
+        application.registerForRemoteNotifications()
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        
+    }
+    
+    // If the app is in the background, and "content-available" == true
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
+        
+    }
+    
+    fileprivate func registerForRemoteNotifications(_ application: UIApplication) {
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { (granted, error) in
+                guard error == nil else {
+                    Log.e(error)
+                    return
+                }
+                
+                if granted {
+                    DispatchQueue.main.async {
+                        application.registerForRemoteNotifications()
+                    }
+                }
+            }
+        }
+        else {
+            let settings: UIUserNotificationSettings = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            
+            DispatchQueue.main.async {
+                application.registerUserNotificationSettings(settings)
+            }
+        }
+    }
+}
+
+@available(iOS 10.0, *)
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, openSettingsFor notification: UNNotification?) { }
+    
+    // If the app is in the foreground, the app will call this
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+       
+       
+        
+        completionHandler([.alert, .badge, .sound])
+    }
+    
+    // If the app is in the background, nothing is called until the user taps the notification, at that point, the app will open and call this
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+      
+    }
+}
+
+extension AppDelegate: MessagingDelegate {
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+        InstanceID.instanceID().instanceID { (result, error) in
+            if let error = error {
+                print(error)
+                
+            } else if let result = result {
+                print(result.token)
+                
+            }
+        }
+    }
+}
