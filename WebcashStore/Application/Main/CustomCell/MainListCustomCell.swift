93ab0d63e4bca9b898e304f1a086ad38269676ad
bunnavitou <laybunnavitou@gmail.com>
2020-02-04 18:19:05 +0900

    add cache for image color after download.

--- WebcashStore/Application/Main/CustomCell/MainListCustomCell.swift
+++ WebcashStore/Application/Main/CustomCell/MainListCustomCell.swift
@@ -19,39 +19,58 @@ class MainListCustomCell: UITableViewCell {
     @IBOutlet weak var appNameLbl: UILabelDynamicSizeClass!
     @IBOutlet weak var wrapperView: DesignableView!
     
-    
-    override func prepareForReuse() {
-//        self.wrapperView.backgroundColor = self.wrapperView.backgroundColor 
-    }
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+    // define as cache for after downloading the image color 
+    let cache = NSCache<NSString, UIColor>()
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
-    
-    
-    func configCell(data : MainModel.Response) {
-        self.appImage.sd_imageIndicator = SDWebImageActivityIndicator.gray
         
-        let url = URL(string: data.app_image ?? "")
-        self.appImage.sd_setImage(with: url) { (img, err, _, _) in
-            if err == nil {
-                img?.getColors({ (imgColor) in
-                    self.wrapperView.backgroundColor = imgColor?.background
-                    self.changeBtnBackgroundColor(color: self.wrapperView.backgroundColor)
-                })
-            } else {
-                self.appImage.image = UIImage(named: "image_placeholder")
-                self.wrapperView.backgroundColor = UIColor(hexString: "F3F4EF")
-                self.changeBtnBackgroundColor(color: self.wrapperView.backgroundColor)
-            }
-        }
+    func configCell(data : MainModel.Response) {
+        
+//        self.appImage.sd_imageIndicator = SDWebImageActivityIndicator.gray
+//        let url = URL(string: data.app_image ?? "")
+//        self.appImage.sd_setImage(with: url) { (img, err, _, _) in
+//            if err == nil {
+//                img?.getColors({ (imgColor) in
+//                    self.wrapperView.backgroundColor = imgColor?.background
+//                    self.changeBtnBackgroundColor(color: self.wrapperView.backgroundColor)
+//                })
+//            } else {
+//                self.appImage.image = UIImage(named: "image_placeholder")
+//                self.wrapperView.backgroundColor = UIColor(hexString: "FFFFFF")
+//                self.changeBtnBackgroundColor(color: self.wrapperView.backgroundColor)
+//            }
+//        }
         
         self.appNameLbl.text = data.app_name
+        
+        let key = NSString(string: data.app_image ?? "")
+
+        // share to cache when every reuse data as array cache
+        if cache.object(forKey: key) != nil{
+            self.wrapperView.backgroundColor = cache.object(forKey: key)
+            self.changeBtnBackgroundColor(color: self.wrapperView.backgroundColor)
+        }else{
+            self.appImage.sd_imageIndicator = SDWebImageActivityIndicator.gray
+            let url = URL(string: key as String)
+            self.appImage.sd_setImage(with: url) { (img, err, _, _) in
+                DispatchQueue.main.async {
+                    if err == nil {
+                        img?.getColors({ (imgColor) in
+                            self.wrapperView.backgroundColor = imgColor?.background
+                            self.changeBtnBackgroundColor(color: self.wrapperView.backgroundColor)
+                            self.cache.setObject((imgColor?.background)!, forKey: NSString(string: key))
+                        })
+                    } else {
+                        self.appImage.image = UIImage(named: "image_placeholder")
+                        self.wrapperView.backgroundColor = UIColor(hexString: "FFFFFF")
+                        self.changeBtnBackgroundColor(color: self.wrapperView.backgroundColor)
+                        self.cache.setObject(UIColor(hexString: "FFFFFF"), forKey: NSString(string: key))
+                    }
+                }
+            }
+        }
     }
     
     private func changeBtnBackgroundColor(color : UIColor?) {
