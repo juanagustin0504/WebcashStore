f030bf0edb760aa6ab7a77e23bc75ab8c42d6933
bunnavitou <laybunnavitou@gmail.com>
2020-02-05 17:18:23 +0900

    change some localizable

--- WebcashStore/Application/SideMenu/SideMenuViewController.swift
+++ WebcashStore/Application/SideMenu/SideMenuViewController.swift
@@ -19,20 +19,16 @@ class SideMenuViewController: UIViewController {
     }
     
     //MARK: - properties
-    private let sideMenuArr = [(img_name : "home", title : "Home".localiz()),
-                               (img_name : "recent", title : "Recent".localiz()),
-                               (img_name : "setting", title : "Settings".localiz())]
+    private let sideMenuArr = [(img_name : "home", title : "home".localiz()),
+                               (img_name : "recent", title : "recent_visited".localiz()),
+                               (img_name : "setting", title : "settings".localiz())]
     
-    
-
     //MARK: - view life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-    
-    
-    //MARK - custom functions
+
 }
 
 //MARK: - Tableview data source & delegate
@@ -54,7 +50,7 @@ extension SideMenuViewController : UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.sideMenuViewController.hideViewController()
         self.tableView.deselectRow(at: indexPath, animated: true)
-        
+     
         if indexPath.row == 0 { // Home
             
         } else if indexPath.row == 1 { // Recent
