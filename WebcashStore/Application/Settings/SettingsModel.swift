1b3b512400cfda1abf1a4c639821331473349f44
juanagustin0504 <juanagustin@naver.com>
2020-02-05 11:19:37 +0900

    - add goto SettingsVC

---
+++ WebcashStore/Application/Settings/SettingsModel.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsModel.swift
+//  WebcashStore
+//
+//  Created by Webcash on 2020/02/03.
+//  Copyright © 2020 Webcash. All rights reserved.
+//
+
+import Foundation
---
+++ WebcashStore/Application/Settings/SettingsViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsViewModel.swift
+//  WebcashStore
+//
+//  Created by Webcash on 2020/02/03.
+//  Copyright © 2020 Webcash. All rights reserved.
+//
+
+import Foundation
