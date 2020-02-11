8ce768d2fb68f111289ba563179a3548d2f4b193
bunnavitou <laybunnavitou@gmail.com>
2020-02-11 17:08:12 +0900

    add localiz for voice screen

--- WebcashStore/Application/VoicePopup/VoicePopupViewController.swift
+++ WebcashStore/Application/VoicePopup/VoicePopupViewController.swift
@@ -7,26 +7,35 @@
 //
 
 import UIKit
+import Speech
 
-let kMaxRadius: CGFloat = 150
-let kMaxDuration: TimeInterval = 3
+let kMaxRadius      : CGFloat       = 150
+let kMaxDuration    : TimeInterval  = 3
 
 class VoicePopupViewController: UIViewController {
     
     //MARK: - Outlets
+    @IBOutlet weak var lblTitle: UILabelDynamicSizeClass!
+    @IBOutlet weak var lblDescription: UILabelDynamicSizeClass!
     @IBOutlet weak var effectBlurView: UIVisualEffectView!
     @IBOutlet weak var microPhoneImg: UIImageView!
+    
     //MARK: - properties
     var effectStyle : UIVisualEffect!
-    let pulsator = Pulsator()
-    private lazy var permissionMng = PermissionManager()
+    let pulsator                    = Pulsator()
+    private lazy var permissionMng  = PermissionManager()
     
     //MARK: - view lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        effectStyle = effectBlurView.effect
-        self.effectBlurView.effect = nil
+        
+        lblTitle.text       = "listening".localiz()
+        lblDescription.text = "hold_say_something_for_searching".localiz()
+        
+
+        effectStyle                 = effectBlurView.effect
+        self.effectBlurView.effect  = nil
         self.animateBlurView()
         
         self.setupPulsatorView()
@@ -40,7 +49,6 @@ class VoicePopupViewController: UIViewController {
     
     
     //MARK: - custom methods
-    
     private func requestSpeechPermission() {
         permissionMng.requestSpeechPermission { (result) in
             if result {
@@ -58,42 +66,41 @@ class VoicePopupViewController: UIViewController {
             }
         }
     }
-    
     private func animateBlurView() {
         UIView.animate(withDuration: 0.4) {
             self.effectBlurView.effect = self.effectStyle
         }
     }
-    
     private func setupPulsatorView() {
         self.microPhoneImg.layer.superlayer?.insertSublayer(pulsator, below: microPhoneImg.layer)
         self.setupInitialValues()
-        
     }
-    
     private func setupInitialValues() {
-        pulsator.numPulse = 6
-        pulsator.animationDuration = kMaxDuration
-        pulsator.backgroundColor = UIColor(hex: 707070).cgColor
-        pulsator.radius = kMaxRadius
+        pulsator.numPulse           = 6
+        pulsator.animationDuration  = kMaxDuration
+        pulsator.backgroundColor    = UIColor(hex: 707070).cgColor
+        pulsator.radius             = kMaxRadius
     }
-    
     //MARK: - button action
     @IBAction func closeBtnDidTapped(_ sender: UIButton) {
         self.dismiss(animated: true)
-        
     }
-    
     @IBAction func longPressGestureAction(_ sender: UILongPressGestureRecognizer) {
+        
         switch sender.state {
         case .began:
             print("Long press `begin`")
+            
             pulsator.start()
             self.permissionMng.startSpeechRecognition()
+            
         case .ended:
+            UIDevice.hapticWithStyle(style: .medium)
             print("Long press `end`")
+            
             pulsator.stop()
             self.permissionMng.stopSpeechRecognition()
+
         default:
             break
         }
